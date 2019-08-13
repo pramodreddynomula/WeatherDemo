a74a5b0e47af1881be6f36a4347d51f9dc75ab37
Pramod Reddy Nomula <pramodreddynomula@pramods-macbook-pro.local>
2019-08-13 11:10:26 +0100

    Initial Commit

---
+++ WeatherDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherDemo
+//
+//  Created by Pramod Reddy Nomula on 13/08/2019.
+//  Copyright © 2019 Pramod Reddy Nomula. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
